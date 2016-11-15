819ef12900020d496ed7c87283110d9ff6b42bb0
jmb259 <jmb259@exeter.ac.uk>
2016-11-15 14:33:40 +0000

    Revert "changed sum to sumoftwonums"
    This reverts commit 2930e25f6eb91b8fab59a1d17342354f4ff45481.

--- sumtwonum.c
+++ sumtwonum.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, sumoftwonums;
+    int n1, n2, sum;
     
     printf("Enter two integers: \n");
 
     scanf("%d %d", &n1, &n2);
 
-    sumoftwonums = n1 + n2;
+    sum = n1 + n2;
     
-    printf("%d + %d = %d\n", n1, n2, sumoftwonums);
-	printf("%d\n", sumoftwonums%2);
+    printf("%d + %d = %d\n", n1, n2, sum);
+	printf("%d\n", sum%2);
 	
-	if(sumoftwonums%2==0)
+	if(sum%2==0)
 	{
-		printf("%d is even\n", sumoftwonums);
+		printf("%d is even\n", sum);
 	}
 	else
 	{
-		printf("%d is odd\n", sumoftwonums);
+		printf("%d is odd\n", sum);
 	}
 
     return 0;
