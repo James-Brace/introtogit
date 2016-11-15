0f9f6bdf3e1293f461d0d49a6fb61430885ae409
jmb259 <jmb259@exeter.ac.uk>
2016-11-15 14:10:20 +0000

    tells is sum is odd or even

--- sumtwonum.c
+++ sumtwonum.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, Sum;
+    int n1, n2, sum;
     
-    printf("Enter two integers: ");
+    printf("Enter two integers: \n");
 
     scanf("%d %d", &n1, &n2);
 
-    Sum = n1 + n2;
+    sum = n1 + n2;
     
-    printf("%d + %d = %d", n1, n2, Sum);
+    printf("%d + %d = %d\n", n1, n2, sum);
+	printf("%d\n", sum%2);
+	
+	if(sum%2==0)
+	{
+		printf("%d is even\n", sum);
+	}
+	else
+	{
+		printf("%d is odd\n", sum);
+	}
 
     return 0;
 }
