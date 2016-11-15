ade0acf0118ca7499f81fbb9064a2effeaccba72
jmb259 <jmb259@exeter.ac.uk>
2016-11-15 15:13:41 +0000

    More white space to make it clear

--- sumtwonum.c
+++ sumtwonum.c
@@ -3,9 +3,13 @@ int main()
 {
     int n1, n2, temp, sum;
     
-    printf("Enter two integers: \n");
-
-    scanf("%d %d", &n1, &n2);
+    printf("\nEnter two integers:\n\n");
+	printf("n1 = ");
+	scanf("%d", &n1);
+	printf("\n");
+	printf("n2 = ");
+	scanf("%d", &n2);
+	printf("\n\n");
 
 	if(n1>n2)
 	{
@@ -14,11 +18,11 @@ int main()
 		n2 = temp;
 	}
 	
-	printf("Your numbers in increasing order are: %d, %d\n", n1,n2);
+	printf("Your numbers in increasing order are: %d, %d\n\n", n1,n2);
 	
     sum = n1 + n2;
     
-    printf("%d + %d = %d\n", n1, n2, sum);
+    printf("%d + %d = %d\n\n", n1, n2, sum);
 	
 	if(sum%2==0)
 	{
