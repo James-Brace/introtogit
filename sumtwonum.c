2930e25f6eb91b8fab59a1d17342354f4ff45481
jmb259 <jmb259@exeter.ac.uk>
2016-11-15 14:30:18 +0000

    changed sum to sumoftwonums

--- sumtwonum.c
+++ sumtwonum.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, sum;
+    int n1, n2, sumoftwonums;
     
     printf("Enter two integers: \n");
 
     scanf("%d %d", &n1, &n2);
 
-    sum = n1 + n2;
+    sumoftwonums = n1 + n2;
     
-    printf("%d + %d = %d\n", n1, n2, sum);
-	printf("%d\n", sum%2);
+    printf("%d + %d = %d\n", n1, n2, sumoftwonums);
+	printf("%d\n", sumoftwonums%2);
 	
-	if(sum%2==0)
+	if(sumoftwonums%2==0)
 	{
-		printf("%d is even\n", sum);
+		printf("%d is even\n", sumoftwonums);
 	}
 	else
 	{
-		printf("%d is odd\n", sum);
+		printf("%d is odd\n", sumoftwonums);
 	}
 
     return 0;
